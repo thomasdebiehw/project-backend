6a775f3dd755ae97b1cf699116b9fffb485c9853
Thomas Debie <thomas.debie@student.howest.be>
2019-05-28 19:10:29 +0200

    base for test project

--- test_folder/app.py
+++ test_folder/app.py
@@ -1,11 +1,22 @@
-from flask import Flask
+from flask import Flask, jsonify
+from flask_cors import CORS
+from classes.sensor_ds18b20 import SensorDS18B20
 
 app = Flask(__name__)
+CORS(app)
+
+temperature_sensor = SensorDS18B20()
+endpoint = '/api/v1'
 
 
 @app.route('/')
 def hello_world():
     return 'Hello World!'
+
+
+@app.route(endpoint + '/sensors/temperature')
+def temperature():
+    return jsonify(temperature_sensor.read_temp()), 200
 
 
 if __name__ == '__main__':
