7cecd3210d88a3c500d01e198aa87483cd292025
Thomas Debie <thomas.debie@student.howest.be>
2019-06-10 22:15:07 +0200

    add flask routes and socketio

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
+from flask_socketio import SocketIO
 from classes.database import Database
 from hwinterface import HWInterface
 import signal
@@ -8,6 +9,7 @@ import atexit
 
 app = Flask(__name__)
 CORS(app)
+socketio = SocketIO(app)
 hw = HWInterface()
 
 conn = Database(app=app, user='project', password='ditwachtwoordmagjezekerweten',
@@ -36,12 +38,34 @@ def temperature():
         return jsonify(temperature=val), 200
 
 
-@app.route(endpoint + '/components', methods=['GET'])
+@app.route(endpoint + '/list/components', methods=['GET'])
 def get_components():
     if request.method == 'GET':
         components = conn.get_data(
             "SELECT * FROM component;")
         return jsonify(components), 200
+
+
+@app.route(endpoint + '/list/events', methods=['GET'])
+def get_components():
+    if request.method == 'GET':
+        components = conn.get_data(
+            "SELECT * FROM event ORDER BY idevent DESC LIMIT 100;")
+        return jsonify(components), 200
+
+
+@app.route(endpoint + '/list/measurements', methods=['GET'])
+def get_components():
+    if request.method == 'GET':
+        components = conn.get_data(
+            "SELECT * FROM measurement ORDER BY idmeasurement DESC LIMIT 100;")
+        return jsonify(components), 200
+
+
+@socketio.on("connect")
+def connecting():
+    socketio.emit("connected")
+    print("Connection with client established")
 
 
 if __name__ == '__main__':
